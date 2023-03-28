81580162b1e4fb6e1049b3c975f334cec9cfb324
NarawadeKunal <129145575+NarawadeKunal@users.noreply.github.com>
2023-03-28 11:02:08 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,48 @@
+const logic={
+    inputStr: "",
+    result: "",
+    setString(str){
+        this.inputStr += str;
+    },
+    equalTo(){
+        this.result = eval(this.inputStr);
+        this.inputStr = this.result;
+    },
+    operator(o){
+        switch(o){
+            case "equal":{
+                this.equalTo();
+                break;
+            }
+            case 'clearEntry':{
+                this.clearEntry();
+                break;
+            }
+            case 'clear':{
+                this.clear();
+                break;
+            }
+            default:{
+                this.result = eval(this.inputStr);
+                this.setString(o);
+            }
+        }
+        
+    },
+    clear(){
+        this.inputStr = "";
+        this.result = "";
+    },
+    sqr(val){
+        return val**2;
+    },
+    squr(val){
+        return Math.sqrt(val).toFixed(3);
+    },
+    fraction(val){
+        return (1/val).toFixed(2);
+    },
+    average(val){
+        return val*(this.result*0.01);
+    }
+}
